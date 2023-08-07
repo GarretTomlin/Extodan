401069172bec121f4f51c852089f914a496d2d55
Garret Tomlin <garrettomlin@Garrets-MacBook-Pro.local>
2023-08-06 19:39:26 -0500

    implemented parser

--- tests/parser_test.go
+++ tests/parser_test.go
@@ -1,2 +1,22 @@
 
 package tests 
+
+import (
+
+	"testing"
+
+	"extodan/pkg/lexer"
+	"extodan/pkg/parser"
+)
+func TestParser(t *testing.T) {
+	input := "2 + 3 * (4 - 2)"
+	expectedResult := 8
+
+	lexer := lexer.NewLexer(input)
+	parser := parser.NewParser(lexer)
+
+	result := parser.ParseProgram()
+	if result != expectedResult {
+		t.Errorf("Expected result %d, but got %d", expectedResult, result)
+	}
+}
