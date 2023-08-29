4734ccd85bb959da257421e1e7ca5942685a3c44
garrettomlin <garrettomlin808@gmail.com>
2023-08-29 15:56:03 -0500

    updated parser ast

--- pkg/parser/parser.go
+++ pkg/parser/parser.go
@@ -1,17 +1,124 @@
 package parser
 
 import (
-	"extodan/pkg/lexer"
+	"extodan/pkg/lexer" // Import the lexer package
+	"extodan/pkg/ast"   // Import the ast package
 )
 
+// Parser holds the lexer and the current token.
 type Parser struct {
-	lexer *lexer.Lexer
+	lexer    *lexer.Lexer
+	curToken lexer.Token
 }
 
+// NewParser creates a new parser using a lexer.
 func NewParser(lexer *lexer.Lexer) *Parser {
-	return &Parser{
-		lexer: lexer,
+	parser := &Parser{lexer: lexer}
+	parser.advanceToken()
+	return parser
+}
+
+// advanceToken advances to the next token.
+func (p *Parser) advanceToken() {
+	p.curToken = p.lexer.GetNextToken()
+}
+
+func (p *Parser) parseProgram() ast.Node {
+	programNode := ast.NewNode(ast.NodeProgram, "")
+	for !p.lexer.IsEOF() {
+		statement := p.parseStatement()
+		if statement.Type != ast.NodeMissingExpression {
+			programNode = ast.AddChild(programNode, statement)
+		}
 	}
+	return programNode
 }
 
 
+func (p *Parser) parseFunctionDeclaration() ast.Node {
+	if p.curToken.Type == lexer.TokenKeyword && p.curToken.Value == "func" {
+		p.advanceToken() // Consume 'func'
+
+		// Parse function name and parameters
+		funcName := p.curToken.Value
+		p.advanceToken() // Consume function name
+
+		p.advanceToken() // Consume '('
+		parameters := []ast.Node{} // Store parameters
+		for p.curToken.Type == lexer.TokenIdentifier {
+			parameter := p.parseParameter()
+			parameters = append(parameters, parameter)
+			if p.curToken.Type == lexer.TokenOperator {
+				p.advanceToken() // Consume ','
+			} else {
+				break
+			}
+		}
+		p.advanceToken() // Consume ')'
+
+		// Parse function body
+		p.advanceToken() // Consume 'do'
+		functionBody := p.parseProgram()
+		p.advanceToken() // Consume 'endfunc'
+
+		// Create the function declaration node
+		funcDeclNode := ast.NewNode(ast.NodeFunctionDeclaration, funcName)
+		for _, parameter := range parameters {
+			funcDeclNode = ast.AddChild(funcDeclNode, parameter)
+		}
+		funcDeclNode = ast.AddChild(funcDeclNode, functionBody)
+
+		return funcDeclNode
+	}
+	return ast.NewNode(ast.NodeFunctionDeclaration, "UnsupportedParameter")
+}
+
+func (p *Parser) parseParameter() ast.Node {
+	if p.curToken.Type == lexer.TokenIdentifier {
+		paramName := p.curToken.Value
+		p.advanceToken() // Consume parameter name
+
+		// Create the parameter node
+		paramNode := ast.NewNode(ast.NodeParameter, paramName)
+		return paramNode
+	}
+
+	// Create a placeholder node for unsupported parameter
+	return ast.NewNode(ast.NodeParameter, "UnsupportedParameter")
+}
+
+func (p *Parser) parseStatement() ast.Node {
+	// Currently, only return statements are supported for demonstration purposes
+	if p.curToken.Type == lexer.TokenKeyword && p.curToken.Value == "return" {
+		returnStmt := ast.NewNode(ast.NodeReturnStatement, "")
+		p.advanceToken()
+		expression := p.parseExpression()
+		if expression.Type != ast.NodeMissingExpression {
+			returnStmt = ast.AddChild(returnStmt, expression)
+		} else {
+			// Use the missing expression node to represent a missing expression
+			returnStmt = ast.AddChild(returnStmt, expression)
+		}
+		return returnStmt
+	}
+
+	// Return a placeholder node for unsupported statements
+	return ast.NewNode(ast.NodeReturnStatement, "Unsupported Statement")
+}
+
+func (p *Parser) parseExpression() ast.Node {
+	// For demonstration purposes, let's assume we only parse integers
+	if p.curToken.Type == lexer.TokenInteger {
+		intNode := ast.NewNode(ast.NodeInteger, p.curToken.Value)
+		p.advanceToken()
+		return intNode
+	}
+
+	// Return a placeholder node for unsupported expressions
+	return ast.NewNode(ast.NodeExpression, "Unsupported Expression")
+}
+
+// Parse the program using the provided lexer.
+func (p *Parser) Parse() ast.Node {
+	return p.parseProgram()
+}
--- tests/parser_test.go
+++ tests/parser_test.go
@@ -1,3 +1,61 @@
 
-package tests 
+package tests
 
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"extodan/pkg/lexer"
+	"extodan/pkg/parser"
+	"extodan/pkg/ast"
+)
+
+func TestParser_ParseFunctionDeclaration(t *testing.T) {
+	sourceCode := `
+		func multiple(a, b) do
+			return a + b
+		endfunc
+	`
+	sourceCode = strings.TrimSpace(sourceCode)
+
+	l := lexer.NewLexer(sourceCode)
+	p := parser.NewParser(l)
+	program := p.Parse()
+
+	assert.NotNil(t, program)
+	assert.Equal(t, ast.NodeProgram, program.Type)
+	assert.Len(t, program.Children, 1)
+
+	assert.Equal(t, ast.NodeFunctionDeclaration, program.Children[0].Type)
+	funcDeclNode := program.Children[0]
+
+	assert.Len(t, funcDeclNode.Children, 4) // Function name, parameters, function body
+
+	// Assert other details of the function declaration and its contents
+	assert.Equal(t, "multiple", funcDeclNode.Value) // Check the function name
+
+	// Check the parameters
+	assert.Len(t, funcDeclNode.Children[0].Children, 2)
+	assert.Equal(t, "a", funcDeclNode.Children[0].Children[0].Value)
+	assert.Equal(t, "b", funcDeclNode.Children[0].Children[1].Value)
+
+	// Check the function body
+	assert.Equal(t, ast.NodeProgram, funcDeclNode.Children[1].Type)
+	assert.Len(t, funcDeclNode.Children[1].Children, 1) // Return statement
+
+	// Check the return statement
+	assert.Equal(t, ast.NodeReturnStatement, funcDeclNode.Children[1].Children[0].Type)
+	assert.NotNil(t, funcDeclNode.Children[1].Children[0].Children[0]) // Check that an expression is present
+	assert.Equal(t, ast.NodeExpression, funcDeclNode.Children[1].Children[0].Children[0].Type)
+
+	// Check the specific contents of the expression
+	expression := funcDeclNode.Children[1].Children[0].Children[0]
+	assert.Equal(t, "+", expression.Value) // Check the operator
+	assert.NotNil(t, expression.Children[0]) // Check that left operand is present
+	assert.NotNil(t, expression.Children[1]) // Check that right operand is present
+	assert.Equal(t, ast.NodeIdentifier, expression.Children[0].Type) // Check the left operand type
+	assert.Equal(t, "a", expression.Children[0].Value) // Check the left operand value
+	assert.Equal(t, ast.NodeIdentifier, expression.Children[1].Type) // Check the right operand type
+	assert.Equal(t, "b", expression.Children[1].Value) // Check the right operand value
+}
